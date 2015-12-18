c38cb3218febab2acc7d79052afe3c1b6598aa86
Maria Nita <maria.nita.dn@gmail.com>
2015-12-18 15:24:01 +0200

    Add post for Person

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, json
 from flask.ext.mongoengine import MongoEngine
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 from config import HOST, MONGODB_SETTINGS
 
@@ -9,6 +9,7 @@ app = Flask('Bullets')
 app.config['MONGODB_SETTINGS'] = MONGODB_SETTINGS
 db.init_app(app)
 api = Api(app)
+parser = reqparse.RequestParser()
 
 class Person(db.Document):
     name = db.StringField(max_length=70)
@@ -26,19 +27,53 @@ class Entry(db.Document):
     username = db.StringField(max_length=50)
     info = db.StringField()
 
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('email', action='append')
+parser.add_argument('role', action='append')
+
 class PersonResource(Resource):
+
     def get(self):
-        users = Person.objects
-        raw_data = []
-        for data in users:
-            raw_data.append(data.to_json())
-        return raw_data
+        data_objects = Person.objects
+        return raw_data(data_objects)
+
+    def post(self):
+        args = parser.parse_args()
+        try:
+            person_id = Person(
+                name=args['name'], role=args['role'], email=args['email']).save()
+        except Exception as e:
+            return {'message': "Error: {0}".format(e)}
+
+        return {'message': 'OK'}
+
+class GroupResource(Resource):
+    def get(self):
+        data_objects = Group.objects
+        return raw_data(data_objects)
+
+    def post(self):
+        pass
+
+class EntryResource(Resource):
+    def get(self):
+        data_objects = Entry.objects
+        return raw_data(data_objects)
 
 class HelloResource(Resource):
     def get(self):
         return {'name': 'bullets'}
 
+def raw_data(data_objects):
+    raw_data = []
+    for data in data_objects:
+        raw_data.append(data.to_json())
+    return raw_data
+
 api.add_resource(PersonResource, '/person/')
+api.add_resource(GroupResource, '/group/')
+api.add_resource(EntryResource, '/entry/')
 api.add_resource(HelloResource, '/')
 
 if __name__ == '__main__':
