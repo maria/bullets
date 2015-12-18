753d681188f314c6202ce2c3b15b1f24a40eb7e3
Raluca Stan <raluca.stan@kalon.ro>
2015-12-18 17:13:48 +0200

    Add fetch to group list

--- src/components/groups/list.vue
+++ src/components/groups/list.vue
@@ -13,11 +13,14 @@
 </template>
 
 <script>
+  const apiURL = '';
+
   import listingtable from '../../modules/listing-table.vue'
 
   export default {
     data() {
       return {
+        groups: null,
         searchQuery: '',
         gridColumns: ['name', 'power'],
         gridData: [
@@ -29,8 +32,24 @@
       }
     },
 
+    created: function () {
+      this.fetchData()
+    },
+
     components: {
       listingtable
+    },
+
+    methods: {
+      fetchData: function() {
+        var xhr = new XMLHttpRequest()
+        var self = this
+        xhr.open('GET', apiURL + self.currentBranch)
+        xhr.onload = function () {
+          self.groups = JSON.parse(xhr.responseText)
+        }
+        xhr.send()
+      }
     }
   }
 </script>
