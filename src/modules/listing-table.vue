b9c104c87271a7a5194a8feeb1a80f55b1e520d6
Raluca Stan <raluca.stan@kalon.ro>
2015-12-18 16:50:56 +0200

    Mock a table on groups page

--- src/modules/listing-table.vue
+++ src/modules/listing-table.vue
@@ -108,17 +108,3 @@
     }
   }
 </script>
-
-<!-- var demo = new Vue({
-  el: '#demo',
-  data: {
-    searchQuery: '',
-    gridColumns: ['name', 'power'],
-    gridData: [
-      { name: 'Chuck Norris', power: Infinity },
-      { name: 'Bruce Lee', power: 9000 },
-      { name: 'Jackie Chan', power: 7000 },
-      { name: 'Jet Li', power: 8000 }
-    ]
-  }
-}) -->
