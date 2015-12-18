a49bc694a1635ce907d9bd3f9a6e6391ad041118
Serban Carjan <serban.carjan@kalon.ro>
2015-12-18 15:25:48 +0200

    logo component and started sidebar.vue

---
+++ src/sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-md-4">
+    <logo></logo>
+  </div>
+</template>
+
+<script>
+  export default {
+    replace: false,
+    data() {
+      return {
+        login: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+
+    methods() {
+      attemptLogin: () => {
+        console.log('wat.');
+      }
+    }
+  }
+</script>
